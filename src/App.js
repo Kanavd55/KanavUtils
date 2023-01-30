c43b96c2519504013dc3b56324464361c4764784
unknown <kanavdahat55@gmail.com>
2023-01-30 14:29:20 +0530

    Dark Mode added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+//import About from './components/About';
 
 
 function App() {
+
+  const [mode,setMode]=useState('light');
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#102d46';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
     <>
-      <Navbar title="KanavUtils"/>
+      <Navbar title="KanavUtils" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3' >
-          <TextForm heading="Enter the text to analyze below"/>
+          <TextForm mode={mode} heading="Enter the text to analyze below"/>
+          {/*<About/>*/}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,12 @@ export default function TextForm(props){
         setText(newText);
     }
 
+    const handleCopyText=()=>{
+        let text=document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
     const handleReClick = () => {
         let splitWord = text.split("");
     
@@ -33,22 +39,23 @@ export default function TextForm(props){
     const [text,setText]=useState('');
     return(
         <>
-    <div>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" placeholder="Enter your text here" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <div className="mb-3" style={{color:props.mode==='dark'?'white':'black'}}>
+            <textarea  style={{color:props.mode==='dark'?'white':'black',backgroundColor:props.mode==='dark'?'rgb(52 101 145)':'white'}} className="form-control" placeholder="Enter your text here" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleReClick}>Reverse</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopyText}>Copy Text</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
     </div>
-    <div className="container my-3">
-        <h2>Your Text Summary</h2>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h3>Your Text Summary</h3>
         <p>{text.split(" ").length} words & {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes required to read above words</p>
-        <h2>preview</h2>
-        <p>{text}</p>
+        <h3>Preview</h3>
+        <p>{text.length>0?text:"Enter your text in the textbox above to preview"}</p>
     </div>
     </>
     )
